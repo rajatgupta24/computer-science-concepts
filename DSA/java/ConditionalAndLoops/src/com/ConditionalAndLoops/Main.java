aea6e68df78e813cab48e2ac2808efbd72831117
rajat <rajat2411gupta@gmail.com>
2021-10-05 04:24:03 +0530

    Factorial

---
+++ DSA/java/ConditionalAndLoops/src/com/ConditionalAndLoops/Main.java
@@ -0,0 +1,20 @@
+package com.ConditionalAndLoops;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int out = 1;
+
+        for (int i=0;i<n;i++) {
+            out *= i+1;
+        }
+
+        System.out.println(n + "! is: " + out);
+    }
+}
