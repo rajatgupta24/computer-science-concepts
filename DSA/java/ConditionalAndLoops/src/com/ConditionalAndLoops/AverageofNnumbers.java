da55ef34ba9df96d59209a1a58aacc304bf2732d
rajat <rajat2411gupta@gmail.com>
2021-10-06 04:13:56 +0530

    removing .idea from from main dir

--- DSA/java/ConditionalAndLoops/src/com/ConditionalAndLoops/AverageofNnumbers.java
+++ DSA/java/ConditionalAndLoops/src/com/ConditionalAndLoops/AverageofNnumbers.java
@@ -2,18 +2,10 @@ package com.ConditionalAndLoops;
 
 import java.util.Scanner;
 
-public class Main {
+public class AverageofNnumbers {
     public static void main(String[] args) {
         System.out.println("Enter the number");
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-
-        int out = 1;
-
-        for (int i=0;i<n;i++) {
-            out *= i+1;
-        }
-
-        System.out.println(n + "! is: " + out);
     }
 }
