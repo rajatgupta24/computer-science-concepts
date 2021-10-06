687a85e7e261f1272a8ae23c9b096afdb387e2cf
rajat <rajat2411gupta@gmail.com>
2021-10-07 03:37:53 +0530

    power in java

--- DSA/java/ConditionalAndLoops/src/com/ConditionalAndLoops/powerInJava.java
+++ DSA/java/ConditionalAndLoops/src/com/ConditionalAndLoops/powerInJava.java
@@ -1,2 +1,20 @@
-package com.ConditionalAndLoops;public class powerInJava {
+package com.ConditionalAndLoops;
+
+import java.util.Scanner;
+
+public class powerInJava {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner num = new Scanner(System.in);
+        int n = num.nextInt();
+        System.out.println("Enter the power");
+        Scanner pow = new Scanner(System.in);
+        int p = pow.nextInt();
+        int power=1;
+        for (int i=1;i<=p;i++) {
+            power *= n;
+        }
+
+        System.out.println(power);
+    }
 }
